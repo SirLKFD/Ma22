bb04dcd0238afa9ab9fe093000a59a43de5e8119
plenngaul <astilleroglennpaul4212@gmail.com>
2025-07-02 16:30:17 +0800

    Added UserTopicController

---
+++ ASI.Basecode.WebApp/Controllers/UserTopicController.cs
@@ -0,0 +1,24 @@
+﻿using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.ServiceModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class UserTopicController : Controller
+    {
+        private readonly ITopicService _topicService;
+
+        public UserTopicController(ITopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        [HttpGet]
+        public IActionResult Topics(int trainingId)
+        {
+            var topics = _topicService.GetAllTopicsByTrainingId(trainingId);
+            return View("~/Views/UserTopics/Topics.cshtml", topics);
+        }
+    }
+}
