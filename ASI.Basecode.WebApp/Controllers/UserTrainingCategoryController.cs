04c5b6c1e8cbfcf8504e7b11958f0e0f8d0b2e07
Lord Kent F. Dinampo <lordkentdinampo@gmail.com>
2025-07-02 17:08:46 +0800

    Added ControllerViews

---
+++ ASI.Basecode.WebApp/Controllers/UserTrainingCategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class UserTrainingCategoryController : Controller
+    {
+        public IActionResult UserTrainingCategory()
+        {
+            return View();
+        }
+    }
+}
