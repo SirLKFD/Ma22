ce5c9d1c85eb1217fa6ecdd2787caa60d28fca43
Lord Kent F. Dinampo <lordkentdinampo@gmail.com>
2025-07-07 00:16:05 +0800

    Added initial category filter function in user side

--- ASI.Basecode.WebApp/Controllers/UserTrainingController.cs
+++ ASI.Basecode.WebApp/Controllers/UserTrainingController.cs
@@ -3,6 +3,7 @@ using ASI.Basecode.Services.ServiceModels;
 using ASI.Basecode.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
@@ -21,6 +22,32 @@ namespace ASI.Basecode.WebApp.Controllers
         {
             var trainings = _trainingService.GetAllTrainings();
             return View("~/Views/UserTrainings/Trainings.cshtml", trainings);
+        }
+
+        [HttpGet]
+        public IActionResult LoadMoreTrainings(int skip = 9)
+        {
+            const int pageSize = 9;
+            var allTrainings = _trainingService.GetAllTrainings();
+            var moreTrainings = allTrainings.Skip(skip).Take(pageSize);
+            return PartialView("~/Views/User/_TrainingCardsPartial.cshtml", moreTrainings);
+        }
+
+        [HttpGet]
+        public IActionResult TrainingsByCategory(int categoryId)
+        {
+            var trainings = _trainingService.GetAllTrainingsByCategoryId(categoryId);
+            return PartialView("~/Views/User/_TrainingCardsPartial.cshtml", trainings);
+        }
+
+        [HttpGet]
+        public IActionResult SearchTrainings(string search)
+        {
+            var allTrainings = _trainingService.GetAllTrainings();
+            var filtered = string.IsNullOrWhiteSpace(search)
+                ? allTrainings
+                : allTrainings.Where(t => t.TrainingName != null && t.TrainingName.ToLower().Contains(search.ToLower())).ToList();
+            return PartialView("~/Views/User/_TrainingCardsPartial.cshtml", filtered);
         }
 
         //public IActionResult UserTrainingTopics(int trainingId)
